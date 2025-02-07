801942adf78bf3489444a259befdca43ba19c04c
Miłosz Bargieł <miloszbargiel@gmail.com>
2025-02-07 21:37:18 +0100

    Use pygame instead of opencv

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import cv2
 import numpy as np
 import skimage.measure
-import PIL.Image
-import PIL.ImageFont
-import PIL.ImageDraw
+import pygame
 
-# higher reduce = less details and more speed, 15-20 is a recommended value
-reduce_pixels = 15
+reduce_pixels = 8
 color_density_str = "@80GCLft1i;:,. "
 max_val = 255
-font = PIL.ImageFont.load_default(size=reduce_pixels)
 contrast_factor = 128
 contrast = 259 * (contrast_factor + 255) / (255 * (259 - contrast_factor))
 contrast_image = np.vectorize(lambda x: min(255, max(0, contrast * (x - 128) + 128)), otypes=[np.uint8])
 color_density = np.vectorize(lambda x: color_density_str[int(x / max_val * (len(color_density_str) - 1))])
+
+pygame.init()
+font = pygame.font.SysFont("monospace", reduce_pixels,  bold=True)
+screen = pygame.display.set_mode((1366, 768), pygame.DOUBLEBUF)
+pygame.display.set_caption("ASCII Camera")
 
 
 def frame_to_ascii(frame):
@@ -22,13 +23,13 @@ def frame_to_ascii(frame):
 
 def ascii_to_image(ascii_frame):
     w, h = ascii_frame.shape
-    image = PIL.Image.new('RGB', (h * reduce_pixels, w * reduce_pixels), (0, 0, 0))
-    draw = PIL.ImageDraw.Draw(image)
+    screen.fill((0, 0, 0))
     for i in range(w):
         for j in range(h):
-            draw.text((j * reduce_pixels, i * reduce_pixels,), ascii_frame[i, j], font=font, fill=(255, 255, 255))
-
-    return np.array(image)
+            char = ascii_frame[i, j]
+            text_surface = font.render(char, True, (255, 255, 255))
+            screen.blit(text_surface, (j * reduce_pixels, i * reduce_pixels))
+    pygame.display.flip()
 
 
 def main():
@@ -36,20 +37,22 @@ def main():
 
     while True:
         ret, frame = camera.read()
+        if not ret:
+            break
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         frame = skimage.measure.block_reduce(frame, (reduce_pixels, reduce_pixels), np.max)
         ascii_map = frame_to_ascii(frame)
-        image = ascii_to_image(ascii_map)
+        ascii_to_image(ascii_map)
 
-        if not ret:
-            break
-        cv2.imshow("ASCII camera (q - quit)", image)
-        key = cv2.waitKey(1)
-        if key == ord('q'):
-            break
-
-    camera.release()
-
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                camera.release()
+                pygame.quit()
+                return
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_q:
+                camera.release()
+                pygame.quit()
+                return
 
 if __name__ == "__main__":
     main()
